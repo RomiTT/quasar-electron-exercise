7508238651f56291a8c5f964b3fbf3c805e6c7ba
RomiTT <chikyom.kim@gmail.com>
2019-05-16 02:42:42 +0900

    Implemented QScrollablePage component which is extended from QPage.

---
+++ src/components/QScrollablePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page v-bind:id="pageId" style="overflow:auto;">
+    <slot></slot>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "QScrollablePage",
+  data () {
+    return {}
+  },
+  props: {
+    pageId: {
+      type: String,
+      default: "",
+      required: true
+    },
+    mainHeaderId: {
+      type: String,
+      default: "",
+      required: true
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted () {
+    var header = document.getElementById(this.mainHeaderId)
+    var page = document.getElementById(this.pageId)
+    page.style.height= `${window.innerHeight-header.offsetHeight}px`
+
+    window.onresize = () => {
+      page.style.height= `${window.innerHeight-header.offsetHeight}px`
+    }
+  }
+}
+</script>
+
+<style>
+</style>
