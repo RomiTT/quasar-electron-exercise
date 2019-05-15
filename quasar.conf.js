ba5333f7d88fe51e82778a4611f5f31bd0729f40
RomiTT <chikyom.kim@gmail.com>
2019-05-15 18:51:37 +0900

    Add QSeparator component to the quasar configuration.

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,8 @@ module.exports = function(ctx) {
         "QTooltip",
         "QBar",
         "QMenu",
-        "QSeparator"
+        "QSeparator",
+        "QScrollArea"
       ],
 
       directives: ["Ripple", "ClosePopup"],
